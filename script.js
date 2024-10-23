a07a5e98ad6feb6b067cd102786ed1de897d5260
Camila Liston <84218260+CAMILALISTON@users.noreply.github.com>
2024-10-22 22:15:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let bancoDeDados = [];
 function add() {
     let usuario = document.getElementById("username").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if (pos == -1) {
+    if (existe(usuario) == false) {
         bancoDeDados.push(usuario.toUpperCase());
         alert("Usuário cadastrado com sucesso!");
         document.getElementById("username").value = null;
@@ -14,12 +13,48 @@ function add() {
 
 function login(){
     let usuario = document.getElementById("user").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if (pos == -1) {
+    if (existe(usuario) ==  false) {
         alert("Usuário não encontrado!");
     }
     else{
         alert("Bem vindo ao Sistema "+ usuario + "!");
     }
+    document.getElementById("username").value = null;
 }
 
+function remove(){
+    let usuario = document.getElementById("userDelete").value;
+    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
+    if (pos == -1) {
+        alert("Usuário não encontrado!");
+    }
+    else{
+        bancoDeDados.splice(pos, 1);
+        alert("Usuário excluido com sucesso!");
+    }
+    document.getElementById("userDelete").value = null;
+}
+
+function edit(){
+    let username = document.getElementById("userEdit").value;
+    if(existe(username) == -1){
+    alert("Usuário não encontrado no banco de dados!");
+    }
+    else{
+        let novoValor = prompt("Digite o novo usuário").toUpperCase();
+        bancoDeDados[pos] = novoValor;
+        alert("Usuário alterado com sucesso!")
+    }
+    document.getElementById("userEdit").value = null;
+
+}
+
+function existe(username){
+    let pos = bancoDeDados.indexOf(username.toUpperCase());
+    if(pos == -1){
+        return false;
+    }
+    else{
+        return true; 
+    }
+}
